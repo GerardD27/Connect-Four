8f430591d8341705e3db1cc0240d9372e115e034
Gerard Debold <debold.gerard@gmail.com>
2018-10-05 10:55:04 -0400

    REdfeined Player constructor to account for player number & piece color

--- Player.java
+++ Player.java
@@ -3,19 +3,29 @@ import java.util.Scanner;
 public class Player {
 
     private final String name;
-    private static int playerNumber;
+    private static int counter = 0;
+    private int playerNumber;
+    private final int playerGamePieceColor;
+    private static final int RED = 1;
+    private static final int YELLOW = 2;
     //private Scanner scanner = new Scanner(System.in);
 
     public Player(String name) {
         //Initialize player number to increment based on how many instances there have been of the class
 
+        this.name = name;
+        this.counter++;
+        this.playerNumber = counter;
+        if(playerNumber == 1){
+            playerGamePieceColor = RED;
+        }
 
-        //Should we keep keyboard input restricted to the main/client program only?
+        else{
+            playerGamePieceColor = YELLOW;
+        }
+    }
 
-       /* System.out.println("Please enter your name Player " + this.playerNumber +": ");
-        this.name = scanner.nextLine();*/
-
-       this.name = name;
-
+    public int placeAPiece(){
+        //Returns the column number the player wishes to place a piece in.
     }
 }
