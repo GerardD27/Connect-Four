90a3ca5f7c8b71017e1ab4a28943eea9411fb78a
Gerard Debold <debold.gerard@gmail.com>
2018-10-18 12:38:35 -0400

    removed unnecessary data fields, removed function to place a piece from Player Class

--- Player.java
+++ Player.java
@@ -5,9 +5,7 @@ public class Player {
     private final String name;
     private static int counter = 0;
     private int playerNumber;
-    private final int playerGamePieceColor;
-    private static final int RED = 1;
-    private static final int YELLOW = 2;
+
     //private Scanner scanner = new Scanner(System.in);
 
     public Player(String name) {
@@ -16,13 +14,6 @@ public class Player {
         this.name = name;
         this.counter++;
         this.playerNumber = counter;
-        if(playerNumber == 1){
-            playerGamePieceColor = RED;
-        }
-
-        else{
-            playerGamePieceColor = YELLOW;
-        }
     }
 
     public String getName() {
@@ -33,10 +24,4 @@ public class Player {
         return playerNumber;
     }
 
-    public int placeAPiece(){
-        //Returns the column number the player wishes to place a piece in.
-
-        //Tempoarary and only for testing purposes
-        return 0;
-    }
 }
